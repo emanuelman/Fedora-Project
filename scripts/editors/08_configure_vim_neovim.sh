c89efe7cc5b8e20e70e6faf4e97b2347f64d35f5
Emanuel Antunes <72533265+emanuelman@users.noreply.github.com>
2024-09-19 13:16:34 -0300

    Update 08_configure_vim_neovim.sh

--- scripts/editors/08_configure_vim_neovim.sh
+++ scripts/editors/08_configure_vim_neovim.sh
@@ -23,21 +23,10 @@ function configure_vim_neovim() {
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     check_error
 
-    # Verificar se o diretório com as configurações personalizadas existe
-    if [ -d "$HOME/Projeto-Fedora" ]; then
-        echo "Copiando arquivos de configuração para o Neovim..."
-        cp $HOME/Desktop/Projeto-Fedora/scripts/editors/init.vim $HOME/.config/nvim/init.vim
-        check_error
-        cp $HOME/Desktop/Projeto-Fedora/scripts/editors/coc-settings.json $HOME/.config/nvim/coc-settings.json
-        check_error
-
-        # Executar PlugInstall automaticamente
-        echo "Executando :PlugInstall no Neovim..."
-        nvim --headless +PlugInstall +qall
-        check_error
-    else
-        echo "Diretório $HOME/Desktop/Projeto-Fedora/scripts/editors não encontrado. Pular a configuração do Neovim."
-    fi
+    # Configuração do Vim:
+    echo "Configurando Nvim"
+    git clone https://github.com/magidc/nvim-config.git ~/.config/nvim
+    check_error
 
     # Configuração do Vim:
     echo "Configurando Vim..."
